936be83ac8f41acdeb792f8289b16fa99497e0d0
リボルバー　オセロット <kyouheihaya@icloud.com>
2017-02-11 15:16:42 +0900

    Initial Commit

---
+++ syousetutoukou kairyou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  syousetutoukou kairyou
+//
+//  Created by リボルバー　オセロット on 2017/02/11.
+//  Copyright © 2017年 ocelot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
